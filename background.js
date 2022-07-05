441059b649f62eda29d349b9a63a40a8f4655bc7
Kamron Bhavnagri <kamwithk@tuta.io>
2022-07-06 03:36:00 +1000

    Update basic storage properties for new lines

--- background.js
+++ background.js
@@ -33,8 +33,8 @@ function connectionOpened(event) {
 
 function lineFetched(event) {
     // Start by getting a timestamp for accuracy
-    rn = new Date();
-    date = rn.getFullYear() + "/" + rn.getMonth() + "/" + rn.getDate();
+    rn = new Date()
+    date = rn.getFullYear() + "/" + rn.getMonth() + "/" + rn.getDate()
     time = rn.getTime()
 
     // Parse provided data
@@ -44,18 +44,29 @@ function lineFetched(event) {
     console.log("Raw Data: ", data)
 
     // Update or create a game entry
-    chrome.storage.local.get(data["process_path"], function(result) {
-        // Create a new game entry if one doesn't exist already
-        // TODO: Otherwise update the existing one
-        if (Object.keys(result).length === 0) {
+    // Each game is stored as numerous small chunks of data
+    updatedGameEntry(data["process_path"], data["sentence"], date, time)
+}
+
+function updatedGameEntry(process_path, line, date, time) {
+    chrome.storage.local.get(process_path, function(game_entry) {
+        if (Object.keys(game_entry).length === 0) {
             var game_entry = {}
-            game_entry[data["process_path"]] = newGameEntry(data["process_path"], date)
-            game_entry[data["process_path"] + "_" + date] = newDateEntry(data["sentence"], time)
-            game_entry[data["process_path"] + "_lines"] = {0: data["sentence"]}
-            chrome.storage.local.set(game_entry);
+            game_entry[process_path] = newGameEntry(process_path, date)
+            game_entry[process_path + "_" + date] = newDateEntry(line, time)
+            game_entry[process_path + "_lines"] = {0: line}
         } else {
+            if (!game_entry[process_path]["dates_read_on"].includes(date)) {
+                game_entry[process_path]["dates_read_on"].push(date)
+            }
+            game_entry[process_path]["last_line_added"] += 1
+
+            game_entry[process_path + "_" + date] = updateGameDateEntry(process_path, line, date, time)
+            game_entry[process_path + "_lines"] = updateGameLines(process_path, line, game_entry[process_path]["last_line_added"])
         }
-    });
+
+        chrome.storage.local.set(game_entry)
+    })
 }
 
 function newGameEntry(name, date) {
@@ -63,7 +74,7 @@ function newGameEntry(name, date) {
         "name": name,
         "dates_read_on": [date],
         "last_line_added": 0
-    };
+    }
 
     return game_entry
 }
@@ -76,6 +87,26 @@ function newDateEntry(line, time) {
         "last_line_recieved": time,
         "last_session_start": time
     }
+}
+
+function updateGameDateEntry(process_path, line, date, time) {
+    chrome.storage.local.get(process_path + "_" + date, function(game_date_entry) {
+        game_date_entry[process_path + "_" + date]["lines_read"] += lineSplitCount(line)
+        game_date_entry[process_path + "_" + date]["chars_read"] += charsInLine(line)
+        // TODO: Add to time read
+        game_date_entry[process_path + "_" + date]["last_line_recieved"] += time
+        // TODO: Replace last_session_start if necessary
+
+        chrome.storage.local.set(game_date_entry)
+    });
+}
+
+function updateGameLines(process_path, line, line_id) {
+    chrome.storage.local.get(process_path + "_lines", function(game_lines_entry) {
+        game_lines_entry[process_path + "_lines"][line_id] = line
+
+        chrome.storage.local.set(game_lines_entry)
+    })
 }
 
 function lineSplitCount(line) {

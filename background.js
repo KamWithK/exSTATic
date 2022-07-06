0033f987153499a3b5288abac1689e3a6fe70f11
Kamron Bhavnagri <kamwithk@tuta.io>
2022-07-06 13:11:01 +1000

    Get and store multiple entries per query

--- background.js
+++ background.js
@@ -49,24 +49,32 @@ function lineFetched(event) {
 }
 
 function updatedGameEntry(process_path, line, date, time) {
-    chrome.storage.local.get(process_path, function(game_entry) {
-        if (Object.keys(game_entry).length === 0) {
-            var game_entry = {}
-            game_entry[process_path] = newGameEntry(process_path, date)
-            game_entry[process_path + "_" + date] = newDateEntry(line, time)
-            game_entry[process_path + "_lines"] = {0: line}
-        } else {
-            if (!game_entry[process_path]["dates_read_on"].includes(date)) {
-                game_entry[process_path]["dates_read_on"].push(date)
+    chrome.storage.local.get(
+        [process_path, process_path + "_" + date, process_path + "_lines"],
+        function(game_entry) {
+            if (Object.keys(game_entry).length === 0) {
+                var game_entry = {}
+                game_entry[process_path] = newGameEntry(process_path, date)
+                game_entry[process_path + "_" + date] = newDateEntry(line, time)
+                game_entry[process_path + "_lines"] = {0: line}
+            } else {
+                if (!game_entry[process_path]["dates_read_on"].includes(date)) {
+                    game_entry[process_path]["dates_read_on"].push(date)
+                }
+                game_entry[process_path]["last_line_added"] += 1
+
+                game_entry[process_path + "_" + date]["lines_read"] += lineSplitCount(line)
+                game_entry[process_path + "_" + date]["chars_read"] += charsInLine(line)
+                // TODO: Add to time read
+                game_entry[process_path + "_" + date]["last_line_recieved"] = time
+                // TODO: Replace last_session_start if necessary
+
+                game_entry[process_path + "_lines"][game_entry[process_path]["last_line_added"]] = line
             }
-            game_entry[process_path]["last_line_added"] += 1
 
-            game_entry[process_path + "_" + date] = updateGameDateEntry(process_path, line, date, time)
-            game_entry[process_path + "_lines"] = updateGameLines(process_path, line, game_entry[process_path]["last_line_added"])
+            chrome.storage.local.set(game_entry)
         }
-
-        chrome.storage.local.set(game_entry)
-    })
+    )
 }
 
 function newGameEntry(name, date) {
@@ -87,26 +95,6 @@ function newDateEntry(line, time) {
         "last_line_recieved": time,
         "last_session_start": time
     }
-}
-
-function updateGameDateEntry(process_path, line, date, time) {
-    chrome.storage.local.get(process_path + "_" + date, function(game_date_entry) {
-        game_date_entry[process_path + "_" + date]["lines_read"] += lineSplitCount(line)
-        game_date_entry[process_path + "_" + date]["chars_read"] += charsInLine(line)
-        // TODO: Add to time read
-        game_date_entry[process_path + "_" + date]["last_line_recieved"] += time
-        // TODO: Replace last_session_start if necessary
-
-        chrome.storage.local.set(game_date_entry)
-    });
-}
-
-function updateGameLines(process_path, line, line_id) {
-    chrome.storage.local.get(process_path + "_lines", function(game_lines_entry) {
-        game_lines_entry[process_path + "_lines"][line_id] = line
-
-        chrome.storage.local.set(game_lines_entry)
-    })
 }
 
 function lineSplitCount(line) {

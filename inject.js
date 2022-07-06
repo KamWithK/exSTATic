81ea906318b2475c3341a56ebdf2e62461e8ddc9
Kamron Bhavnagri <kamwithk@tuta.io>
2022-07-06 22:49:42 +1000

    Insert new lines into injected page

--- inject.js
+++ inject.js
@@ -14,15 +14,34 @@ function parseLineKey(key, old_value, new_value) {
     return false
 }
 
+function insertLine(line) {
+    entry_holder = document.getElementById("entry_holder")
+    
+    new_div = document.createElement("div")
+    new_svg = document.createElement("svg")
+    new_p = document.createElement("p")
+    
+    new_div.classList.add("sentence-entry")
+    new_svg.classList.add("circle-bullet-point")
+    new_p.classList.add("sentence")
+    
+    new_p.innerHTML = line
+    
+    entry_holder.appendChild(new_div)
+    new_div.appendChild(new_svg)
+    new_div.appendChild(new_p)
+}
+
 chrome.storage.local.onChanged.addListener(function (changes, _) {
     for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
         key = parseLineKey(key, oldValue, newValue)
+        
         if (key) {
             process_path = key[0]
             line_id = key[1]
             line = newValue
 
-            console.log("Process Path: ", process_path, "Line ID: ", line_id, "Line: ", line)
+            insertLine(line)
         }
     }
 })

1d2fb19276fbad98cc01e966c8a7e9bf6ac1eeeb
Kamron Bhavnagri <kamwithk@tuta.io>
2022-07-10 14:13:17 +1000

    Fixed line split

--- src/calculations.js
+++ src/calculations.js
@@ -1,10 +1,11 @@
 // Source of information - https://en.wikipedia.org/wiki/List_of_Japanese_typographic_symbols
 var IGNORE = /[〔〕《》〖〗〘〙〚〛【】「」［］『』｛｝\[\]()（）｟｠〈〉≪≫。、.,※＊'：！?？‥…―─ｰ〽～→♪♪ ♫ ♬ ♩\"　\t\n]/g
+var SPLIT = /[\n。.！?？]/g
 
 export function charsInLine(line) {
     return line.replaceAll(IGNORE, "").length
 }
 
 export function lineSplitCount(line) {
-    return line.split("\n").split("。").length
+    return line.split(SPLIT).length
 }

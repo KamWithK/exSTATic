3f0062a0a0bbca417544d4e38b7a279f0f9169d3
Kamron Bhavnagri <kamwithk@tuta.io>
2022-08-14 14:47:20 +1000

    Cleaner d3 imports

--- src/stats/stats_inject.ts
+++ src/stats/stats_inject.ts
@@ -1,7 +1,7 @@
 console.log("Injected")
 
 import { getData } from "../data_wrangling/data_extraction"
-import * as d3 from "d3"
+import {group, rollup, sum} from "d3-array"
 
 const SECS_TO_HRS = 60 * 60
 
@@ -16,11 +16,11 @@ let read_speed_per
 const setup = async () => {
     data = await getData()
 
-    uuid_groups = d3.group(data, d => d.uuid)
+    uuid_groups = group(data, d => d.uuid)
 
-    time_read_per = d3.rollup(data, v => d3.sum(v, d => d.time_read / SECS_TO_HRS), d => d.uuid)
-    chars_read_per = d3.rollup(data, v => d3.sum(v, d => d.chars_read), d => d.uuid)
-    read_speed_per = d3.rollup(data, v => d3.sum(v, d => d.read_speed * SECS_TO_HRS), d => d.uuid)
+    time_read_per = rollup(data, v => sum(v, d => d.time_read / SECS_TO_HRS), d => d.uuid)
+    chars_read_per = rollup(data, v => sum(v, d => d.chars_read), d => d.uuid)
+    read_speed_per = rollup(data, v => sum(v, d => d.read_speed * SECS_TO_HRS), d => d.uuid)
 }
 setup()
 

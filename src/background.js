4483acc9744701e68dd536348c4554c94050d7f5
Kamron Bhavnagri <kamwithk@tuta.io>
2022-08-24 13:59:15 +1000

    Migration from Manifest V2 to V3

--- src/background.js
+++ src/background.js
@@ -1,18 +1,33 @@
 console.log("exSTATic")
 
 import { message_action } from "./messaging/message_actions"
-import { SocketManager } from "./messaging/socket_actions"
+import { connectionClosed, connectionOpened, dataFetched, messagingConnected } from "./messaging/socket_actions"
+
+import ReconnectingWebSocket from "reconnecting-websocket"
 
 var browser = require("webextension-polyfill")
+
+browser.runtime.onUpdateAvailable.addListener(() => browser.runtime.reload())
+browser.runtime.onInstalled.addListener(async () => {
+    console.log("Reloading all extension tabs...")
+    for (const content_script of chrome.runtime.getManifest().content_scripts) {
+        for (const tab of await chrome.tabs.query({url: content_script.matches})) {
+            browser.scripting.executeScript({
+                target: {tabId: tab.id},
+                func: () => window.location.reload()
+            })
+        }
+    }
+})
 
 // Message passing is used for actions which can only be performed on the background page
 browser.runtime.onMessage.addListener(message_action)
 
-browser.browserAction.onClicked.addListener(async _ => {
+browser.action.onClicked.addListener(async _ => {
     const listen_status = (await browser.storage.local.get("listen_status"))["listen_status"]
 
     if (listen_status == true || listen_status === undefined) {
-        await browser.browserAction.setIcon({
+        await browser.action.setIcon({
             "path": {
                 "100": "docs/disabled_100x100.png",
                 "500": "docs/disabled.png"
@@ -23,7 +38,7 @@ browser.browserAction.onClicked.addListener(async _ => {
             "listen_status": false
         })
     } else {
-        await browser.browserAction.setIcon({
+        await browser.action.setIcon({
             "path": {
                 "100": "docs/favicon_100x100.png",
                 "500": "docs/favicon.png"
@@ -36,4 +51,11 @@ browser.browserAction.onClicked.addListener(async _ => {
     }
 })
 
-new SocketManager("ws://localhost:9001")
+let socket = new ReconnectingWebSocket("ws://localhost:9001")
+
+socket.addEventListener("open", connectionOpened)
+socket.addEventListener("close", connectionClosed)
+socket.addEventListener("error", connectionClosed)
+socket.addEventListener("message", dataFetched)
+
+browser.runtime.onConnect.addListener(messagingConnected)
--- src/messaging/socket_actions.js
+++ src/messaging/socket_actions.js
@@ -3,76 +3,63 @@ import { dateNowString, timeNowSeconds } from "../calculations"
 var browser = require("webextension-polyfill")
 
 export const SPLIT_PATH = /\\|\//g
-var socket
 
-export class SocketManager {
-    constructor(url) {
-        this.url = url
+let port = undefined
 
-        this.port = undefined
-        browser.runtime.onConnect.addListener(this.messagingConnected.bind(this))
+export function connectionOpened() {
+    console.log("Connected")
+}
 
-        this.connectToWebSocket()
+export function connectionClosed() {
+    console.log("Connection Lost")
+}
+
+export function messagingConnected(port_) {
+    console.log("Messaging Connected: ", port_)
+    port = port_
+
+    port.onDisconnect.addListener(messagingDisconnected)
+}
+
+function messagingDisconnected(port_) {
+    console.log("Messaging Disconnected: ", port_)
+
+    if (port === port_) {
+        port = undefined
+    }
+}
+
+export async function dataFetched(event) {
+    console.log(event)
+    const listen_status = (await browser.storage.local.get("listen_status"))["listen_status"]
+    if (listen_status === false) {
+        return
     }
 
-    messagingConnected(port) {
-        console.log("Messaging Connected: ", port)
-        this.port = port
+    // Start by getting a timestamp for accuracy
+    const time = timeNowSeconds()
+    const date = dateNowString()
 
-        this.port.onDisconnect.addListener(this.messagingDisconnected.bind(this))
-    }
-
-    messagingDisconnected(port) {
-        console.log("Messaging Disconnected: ", port)
-        if (port == this.port) {
-            this.port = undefined
-        }
-    }
-
-    connectToWebSocket() {
-        const socket = new WebSocket(this.url)
-
-        socket.addEventListener("open", this.connectionOpened.bind(this))
-        socket.addEventListener("close", this.connectToWebSocket.bind(this))
-        socket.addEventListener("error", this.connectToWebSocket.bind(this))
-        socket.addEventListener("message", this.dataFetched.bind(this))
-    }
+    // Parse provided data
+    const data = JSON.parse(event.data)
+    console.log("Recieved Socket Data: ", data)
     
-    connectionOpened() {
-        console.log("Connected")
+    // Lines will have a valid process path and sentence
+    if (!data.hasOwnProperty("process_path") || !data.hasOwnProperty("sentence")) {
+        return
     }
-    
-    async dataFetched(event) {
-        const listen_status = (await browser.storage.local.get("listen_status"))["listen_status"]
-        if (listen_status === false) {
-            return
-        }
 
-        // Start by getting a timestamp for accuracy
-        const time = timeNowSeconds()
-        const date = dateNowString()
+    let process_path = data["process_path"]
+    const line = data["sentence"]
 
-        // Parse provided data
-        const data = JSON.parse(event.data)
-        console.log("Recieved Socket Data: ", data)
-        
-        // Lines will have a valid process path and sentence
-        if (!data.hasOwnProperty("process_path") || !data.hasOwnProperty("sentence")) {
-            return
-        }
+    // Only consider at max the last three sections of the path
+    const path_segments = process_path.split(SPLIT_PATH)
+    process_path = path_segments.slice(Math.max(0, path_segments.length - 3)).join("\/")
 
-        let process_path = data["process_path"]
-        const line = data["sentence"]
-
-        // Only consider at max the last three sections of the path
-        const path_segments = process_path.split(SPLIT_PATH)
-        process_path = path_segments.slice(Math.max(0, path_segments.length - 3)).join("\/")
-
-        await this.port.postMessage({
-            "line": line,
-            "process_path": process_path,
-            "date": date,
-            "time": time
-        })
-    }
+    await port.postMessage({
+        "line": line,
+        "process_path": process_path,
+        "date": date,
+        "time": time
+    })
 }

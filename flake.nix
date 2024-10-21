ef550117c946f406a938448eb7717c6834ab0c32
Kamron Bhavnagri <kamwithk@tuta.io>
2024-10-21 21:01:00 +1100

    Nix flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = (import (inputs.nixpkgs) { inherit system; });
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            # pkgs.fnm
+            pkgs.nodePackages.nodejs
+            pkgs.nodePackages.npm
+            pkgs.nodePackages.typescript
+          ];
+        };
+      }
+    );
+}

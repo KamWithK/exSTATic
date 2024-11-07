4130c1cdb7d29652ef987467ee9430b3a6c65763
Kamron Bhavnagri <kamwithk@tuta.io>
2024-11-07 18:13:05 +1100

    Remove bubbling

--- src/components/interface/line_holder.svelte
+++ src/components/interface/line_holder.svelte
@@ -1,33 +1,34 @@
 <script lang="ts">
-  import { createBubbler } from "svelte/legacy";
-
-  const bubble = createBubbler();
   import Line from "./line.svelte";
 
   import { onMount } from "svelte";
 
   interface Props {
     lines: string[][];
+    onclick: () => void;
+    ondblclick: ((this: Window, ev: MouseEvent) => any) | null;
   }
 
-  let { lines }: Props = $props();
-  let entry_holder: HTMLElement = $state();
+  let { lines = $bindable(), onclick, ondblclick }: Props = $props();
+  let entry_holder: HTMLElement | undefined = $state();
 
   const observer = new MutationObserver(() => {
+    if (!entry_holder) return;
     window.scrollTo(0, entry_holder.scrollHeight);
   });
 
-  onMount(() =>
-    observer.observe(entry_holder, { childList: true, subtree: true }),
-  );
+  onMount(() => {
+    if (!entry_holder) return;
+    observer.observe(entry_holder, { childList: true, subtree: true });
+  });
 </script>
 
 <div
   id="entry_holder"
   bind:this={entry_holder}
   role="presentation"
-  onclick={bubble("click")}
-  ondblclick={bubble("dblclick")}
+  {onclick}
+  {ondblclick}
 >
   {#each lines as [_, id, line, time]}
     <Line {id} {time} sentence={line} />

af4043c9797ad1a43a3ab78b313a79e8b3e72181
Kamron Bhavnagri <kamwithk@tuta.io>
2024-11-07 20:38:44 +1100

    Manual scatterplot Svelte 5 migration

--- src/components/charts/scatterplot.svelte
+++ src/components/charts/scatterplot.svelte
@@ -1,4 +1,3 @@
-<!-- @migration-task Error while migrating Svelte code: Encountered an export declaration pattern that is not supported for automigration. -->
 <script lang="ts">
   import LineAxis from "../draw/oriented_axis.svelte";
   import Circles from "../draw/circles.svelte";
@@ -14,90 +13,99 @@
   import { scaleLinear, scaleTime } from "d3-scale";
   import iwanthue from "iwanthue";
   import type { DataEntry } from "../../data_wrangling/data_extraction";
-  import type { ScaleLinear, ScaleTime } from "d3";
 
-  export let data: DataEntry[];
-  export let radius = 60;
-
-  export let x_accessor: (d: Partial<DataEntry>) => Date,
+  interface Props {
+    data: DataEntry[];
+    x_accessor: (d: Partial<DataEntry>) => Date;
     y_accessor: (d: DataEntry) => number;
-  export let r_accessor: ((d: Partial<DataEntry>) => number) | undefined,
+    r_accessor: (d: Partial<DataEntry>) => number;
     c_accessor: (d: Partial<DataEntry>) => string;
+    tooltip_accessors: TooltipAccessors;
+    tooltip_formatters: TooltipFormatters;
+    graph_title: string;
+    x_label: string;
+    y_label: string;
+  }
 
-  export let tooltip_accessors: TooltipAccessors;
-  export let tooltip_formatters: TooltipFormatters;
+  let {
+    data,
+    x_accessor,
+    y_accessor,
+    r_accessor,
+    c_accessor,
+    tooltip_accessors,
+    tooltip_formatters,
+    graph_title,
+    x_label,
+    y_label,
+  }: Props = $props();
 
-  export let graph_title: string;
-  export let x_label: string, y_label: string;
+  let radius = 60;
 
-  let groups: string[], hues: string[];
-  $: groups = Array.from(group(data, c_accessor).keys());
-  $: hues = iwanthue(groups.length, {
-    colorSpace: [0, 360, 0, 100, 50, 100],
-    clustering: "force-vector",
-    seed: "exSTATic!",
+  let groups = $derived(Array.from(group(data, c_accessor).keys()));
+  let hues: string[] = $derived(
+    iwanthue(groups.length, {
+      colorSpace: [0, 360, 0, 100, 50, 100],
+      clustering: "force-vector",
+      seed: "exSTATic!",
+    }),
+  );
+
+  let [height, width, margin] = $state([1000, 1200, 50]);
+  let safeHeight = $derived.by(() => {
+    const minHeight = Math.max(height, 500);
+    return minHeight > width ? width : minHeight;
   });
-
-  let [height, width, margin] = [1000, 1200, 50];
-  $: if (height < 500) height = 500;
-  $: if (width < 500) width = 500;
-  $: if (height > width) height = width;
+  let safeWidth = $derived(Math.max(width, 500));
 
   // Physical ranges shrink in proport to the maximal circle radius and padding
-  let [x_range, y_range]: [[number, number], [number, number]] = [
-    [0, 0],
-    [0, 0],
-  ];
-  $: x_range = [radius + margin, width - radius - margin];
-  $: y_range = [height - radius - margin, radius + margin];
-
-  let x_scale: ScaleTime<number, number, never>;
-  let y_scale: ScaleLinear<number, number, never>;
-  let r_scale: ScaleLinear<number, number, never> | undefined;
+  let x_range = $derived([radius + margin, safeWidth - radius - margin]);
+  let y_range = $derived([safeHeight - radius - margin, radius + margin]);
 
   // Map data (domains) onto physical scales (ranges)
   // Several of these functions can return undefined
   // Lift those up
-  $: {
+  let x_scale = $derived.by(() => {
     const scale_extent = x_accessor && extent(data, x_accessor);
     if (
       scale_extent &&
       scale_extent[0] !== undefined &&
       scale_extent[1] !== undefined
     ) {
-      x_scale = scaleTime().domain(scale_extent).range(x_range).nice();
+      return scaleTime().domain(scale_extent).range(x_range).nice();
     }
-  }
-  $: {
+  });
+  let y_scale = $derived.by(() => {
     const scale_extent = y_accessor && extent(data, y_accessor);
     if (
       scale_extent &&
       scale_extent[0] !== undefined &&
       scale_extent[1] !== undefined
     ) {
-      y_scale = scaleLinear().domain(scale_extent).range(y_range).nice();
+      return scaleLinear().domain(scale_extent).range(y_range).nice();
     }
-  }
-  $: {
+  });
+  let r_scale = $derived.by(() => {
     const scale_extent = r_accessor && extent(data, r_accessor);
     if (
       scale_extent &&
       scale_extent[0] !== undefined &&
       scale_extent[1] !== undefined
     ) {
-      r_scale = scaleLinear().domain(scale_extent).range([0, radius]);
+      return scaleLinear().domain(scale_extent).range([0, radius]);
     }
-  }
+  });
 
-  const xGet = (d: Partial<DataEntry>) => x_scale(x_accessor(d));
-  const yGet = (d: DataEntry) => y_scale(y_accessor(d));
+  const xGet = (d: Partial<DataEntry>) => x_scale && x_scale(x_accessor(d));
+  const yGet = (d: DataEntry) => y_scale!(y_accessor(d));
   const rGet = (d: DataEntry) =>
     r_accessor && r_scale && r_scale(r_accessor(d));
   const cGet = (d: DataEntry) => hues[groups.indexOf(c_accessor(d))];
 
   const [x_formatter, y_formatter] = [timeFormat("%B\n%Y"), format(".2s")];
 
-  let mouse_move: (event: MouseEvent) => void, mouse_out: () => void;
+  let mouse_move: (event: MouseEvent) => void = $state(),
+    mouse_out: () => void = $state();
 </script>
 
 <div class="flex h-full w-full flex-col items-center bg-slate-900 p-12">
@@ -113,22 +121,22 @@
       width="100%"
       class="max-h-[80vh]"
       style="resize: both;"
-      viewBox="0 0 {width} {height}"
+      viewBox="0 0 {safeWidth} {safeHeight}"
       preserveAspectRatio="xMidYMid meet"
     >
       <LineAxis
-        bind:scale={x_scale}
-        bind:height
-        bind:width
+        scale={x_scale}
+        height={safeHeight}
+        width={safeWidth}
         {margin}
         position="bottom"
         formatter={x_formatter}
         label={x_label}
       />
       <LineAxis
-        bind:scale={y_scale}
-        bind:height
-        bind:width
+        scale={y_scale}
+        height={safeHeight}
+        width={safeWidth}
         {margin}
         position="left"
         formatter={y_formatter}
@@ -143,8 +151,8 @@
         {cGet}
         {x_scale}
         {y_scale}
-        {mouse_move}
-        {mouse_out}
+        bind:mouse_move
+        bind:mouse_out
       />
     </svg>
 

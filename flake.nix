07e1a814d801fd4280dca189a01ab00963060e28
Kamron Bhavnagri <kamwithk@tuta.io>
2024-11-09 23:34:34 +1100

    Patch for vtsls language server

--- flake.nix
+++ flake.nix
@@ -2,6 +2,10 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    vtslsPatch = {
+      url = "https://github.com/NixOS/nixpkgs/pull/347284.patch";
+      flake = false;
+    };
   };
 
   outputs =
@@ -10,14 +14,20 @@
       system:
       let
         pkgs = (import (inputs.nixpkgs) { inherit system; });
+        patchedNixpkgs = pkgs.applyPatches {
+          src = pkgs.path;
+          patches = [ inputs.vtslsPatch ];
+        };
+        patchedPkgs = import patchedNixpkgs { inherit system; };
       in
       {
         devShell = pkgs.mkShell {
-          buildInputs = [
-            # pkgs.fnm
-            pkgs.nodePackages.nodejs
-            pkgs.nodePackages.npm
-            pkgs.nodePackages.typescript
+          buildInputs = with pkgs.nodePackages; [
+            nodejs
+            npm
+            typescript
+            prettier
+            patchedPkgs.vtsls
           ];
         };
       }

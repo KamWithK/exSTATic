c504f6b42c2439725587c7ef0c8c6838ca0f5045
Kamron Bhavnagri <kamwithk@tuta.io>
2024-11-24 15:19:42 +1100

    Wrap TextBoard binary with LD_LIBRARY_PATH

--- flake.nix
+++ flake.nix
@@ -28,21 +28,6 @@
           packages = [
             textboard.defaultPackage.${system}
           ];
-          LD_LIBRARY_PATH =
-            with pkgs;
-            lib.makeLibraryPath [
-              libxkbcommon
-              libGL
-
-              # WINIT_UNIX_BACKEND=wayland
-              wayland
-
-              # WINIT_UNIX_BACKEND=x11
-              xorg.libXcursor
-              xorg.libXrandr
-              xorg.libXi
-              xorg.libX11
-            ];
         };
       }
     );
--- textboard/flake.nix
+++ textboard/flake.nix
@@ -17,9 +17,30 @@
       let
         pkgs = import nixpkgs { inherit system; };
         naersk-lib = pkgs.callPackage naersk { };
+        libPath =
+          with pkgs;
+          lib.makeLibraryPath [
+            libxkbcommon
+            libGL
+
+            # WINIT_UNIX_BACKEND=wayland
+            wayland
+
+            # WINIT_UNIX_BACKEND=x11
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXi
+            xorg.libX11
+          ];
       in
       {
-        defaultPackage = naersk-lib.buildPackage { src = ./.; };
+        defaultPackage = naersk-lib.buildPackage {
+          src = ./.;
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          postInstall = ''
+            wrapProgram "$out/bin/textboard" --prefix LD_LIBRARY_PATH : "${libPath}"
+          '';
+        };
         devShell =
           with pkgs;
           mkShell {
